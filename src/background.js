6bf284d55041a9e329a617526a9e884881759aa3
Moises Baltazar <me@moisesb.com>
2019-07-01 13:06:37 -0500

    Traverse menu tree

--- src/background.js
+++ src/background.js
@@ -2,7 +2,9 @@ function onClickHandler(info, tab) {
     console.log('item ' + info.menuItemId + ' was clicked')
     console.log('info: ' + JSON.stringify(info))
     console.log('tab: ' + JSON.stringify(tab))
-    handlers[info.menuItemId](tab)
+    const [fn, type] = info.menuItemId.replace(' ', '_').split(':')
+    const url = info.linkUrl || info.pageUrl
+    handlers[fn](url, tab)
 }
 
 function matrix(dict, path = []) {
@@ -63,45 +65,29 @@ const menuTree = {
 
 // Set up context menu
 chrome.runtime.onInstalled.addListener(function() {
-    menuItem('Student List ↯', '/users')
-    menuItem('Import Quiz', '/quizzes')
-    menuItem('Export Quiz ↯', '/quizzes/*')
-    menuItem('Add Questions', '/quizzes/*')
-    menuItem('Moderate Quiz', '/quizzes/*')
-    menuItem('Quiz Overrides', '/quizzes/*')
+    for (const urlPattern in menuTree) {
+        for (const fnName in menuTree[urlPattern]) {
+            const fn = menuTree[urlPattern][fnName]
+            handlers[fnName] = fn
+            const title = fnName.replace('_', ' ')
+
+            // Right-click anywhere on page
+            chrome.contextMenus.create({
+                id: `${fnName}:page`,
+                title,
+                contexts: ['page'],
+                documentUrlPatterns: [`*://*/courses/*${urlPattern}`],
+            })
+
+            // Right-click on links
+            chrome.contextMenus.create({
+                id: `${fnName}:link`,
+                title,
+                contexts: ['link'],
+                targetUrlPatterns: [`*://*/courses/*${urlPattern}`],
+            })
+        }
+    }
 })
 
 const handlers = {}
-
-function menuItem(text, urlPattern, onClick) {
-    // ids are required string properties for menu items
-    // but for our purposes, just generate a random name
-    const id1 = Math.random().toString()
-    const id2 = Math.random().toString()
-
-    // provide default click handler
-    if (!onClick)
-        onClick = () => {
-            alert(`Clicked on: ${text}`)
-        }
-
-    // associate random Id to click handler
-    handlers[id1] = onClick
-    handlers[id2] = onClick
-
-    // Right-click anywhere on page
-    chrome.contextMenus.create({
-        id: id1,
-        title: text,
-        contexts: ['page'],
-        documentUrlPatterns: [`*://*/courses/*${urlPattern}`],
-    })
-
-    // Right-click on links
-    chrome.contextMenus.create({
-        id: id2,
-        title: text,
-        contexts: ['link'],
-        targetUrlPatterns: [`*://*/courses/*${urlPattern}`],
-    })
-}
