d1c47cdd395742d271728127db4d9af6f23aaa75
Moises Baltazar <me@moisesb.com>
2019-08-08 18:01:34 -0500

    Move functions to content.js

--- src/background.js
+++ src/background.js
@@ -1,26 +1,3 @@
-function store(obj) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.set(obj, resolve)
-    })
-}
-
-function extractToken(cookie) {
-    const regex = /_csrf_token=(\S+)/
-    const result = regex.exec(cookie)
-    return result ? decodeURIComponent(result[1]) : ''
-}
-
-function setTokenFromCookie() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.executeScript({ code: 'document.cookie' }, cookie => {
-            store({
-                token: extractToken(cookie[0]),
-                date: new Date(),
-            }).then(resolve)
-        })
-    })
-}
-
 function processUrl(url) {
     const regex = /^(https?:\/\/[^\/]+\/)courses\/(?:(\d+)\/?(?:([a-z]+)\/?(\d+)?)?)/
     const [, base, course, type, item] = regex.exec(url) || []
@@ -32,138 +9,65 @@ function processUrl(url) {
 function onClickHandler(info, tab) {
     const [fn, type] = info.menuItemId.replace(' ', '_').split(':')
     const url = info.linkUrl || info.pageUrl
+
     const urlObj = processUrl(url)
     chrome.tabs.executeScript({
-        code: `const baseUrl = "${urlObj.base}", apiUrl = "${urlObj.api}", course_id="${urlObj.course}", item_id="${urlObj.item}", item_type="${urlObj.type}"`,
+        code: `var baseUrl = "${urlObj.base}", apiUrl = "${urlObj.api}", course_id="${urlObj.course}", item_id="${urlObj.item}", item_type="${urlObj.type}", original_url="${url}", exec_fn = "${fn}";`,
     })
-    Promise.all([setTokenFromCookie(), store(urlObj)]).then(function() {
-        handlers[fn](urlObj, tab.id, tab)
-    })
+    chrome.tabs.executeScript({ file: 'content.js' })
+    // handlers[fn](urlObj, tab.id, tab)
 }
-
-function matrix(dict, path = []) {
-    let result = []
-    for (const prop in dict) {
-        const value = dict[prop]
-        if (typeof value == 'object')
-            result = result.concat(matrix(value, path.concat(prop)))
-        else result.push(path.concat(prop, value))
-    }
-    return result
-}
-
-const T = {
-    dict(matrix, keyCol = 0, valueCol) {
-        if (valueCol == undefined) valueCol = keyCol + 1
-
-        const result = {}
-        for (const row of matrix) {
-            result[row[keyCol]] = row[valueCol]
-        }
-        return result
-    },
-
-    slice(matrix, startCol, endCol) {
-        return matrix.map(row => row.slice(startCol, endCol))
-    },
-
-    get(matrix, col) {
-        return matrix.map(row => row[col])
-    },
-
-    eachRow(matrix, fn) {
-        for (const row of matrix) {
-            fn.apply(matrix, row)
-        }
-    },
-}
-
-chrome.contextMenus.onClicked.addListener(onClickHandler)
-
-function loadFiles(...files) {
-    for (const file of files) {
-        chrome.tabs.executeScript({ file })
-    }
-}
-
-function reloadPage() {
-    chrome.tabs.reload()
-}
-
-const menu = matrix({
-    '/users': {
-        Student_List(url, tab) {
-            loadFiles(
-                'assets/axios.v0.19.0.min.js',
-                'downloadCSV.js',
-                'common.js',
-                'exportStudents.js',
-            )
-        },
-    },
-    '/quizzes': {
-        Import_Quiz(url, tab) {},
-    },
-    '/quizzes/*': {
-        Export_Quiz(url, tab) {
-            loadFiles(
-                'assets/jquery.js',
-                'assets/axios.v0.19.0.min.js',
-                'assets/js-yaml.js',
-                'q_lib.js',
-                'canvas_questions.js',
-                'common.js',
-                'exportQuiz.js',
-            )
-        },
-        Add_Questions(url, tab) {
-            loadFiles(
-                'assets/jquery.js',
-                'assets/js-yaml.js',
-                'quizzes.js',
-                'quizPost.js',
-            )
-        },
-        Moderate_quiz(url, tab) {
-            loadFiles(
-                'assets/axios.v0.19.0.min.js',
-                'assets/jquery.js',
-                'common.js',
-                'moderate_quiz.js',
-            )
-        },
-        Quiz_Overrides(url, tab) {
-            loadFiles(
-                'assets/axios.v0.19.0.min.js',
-                'assets/jquery.js',
-                'common.js',
-                'quizAssign.js',
-            )
-        },
-    },
-})
-
-const handlers = T.dict(menu, 1, 2)
 
 // Set up context menu
+
 chrome.runtime.onInstalled.addListener(function() {
-    T.eachRow(menu, (url, id) => {
-        const title = id.replace('_', ' ')
-        const pattern = ['*://*/courses/*' + url]
+    chrome.contextMenus.create({
+        id: 'Student_List',
+        title: 'Student List (.csv)',
+        documentUrlPatterns: ['*://*/courses/*'],
+    })
 
-        // Right-click anywhere on page
-        chrome.contextMenus.create({
-            id: id + ':page',
-            title,
-            documentUrlPatterns: pattern,
-        })
+    chrome.contextMenus.create({
+        id: 'Import_Quiz',
+        title: 'Import a Quiz',
+        documentUrlPatterns: ['*://*/courses/*/quizzes'],
+    })
 
-        // Right-click on a link
-        chrome.contextMenus.create({
-            id: id + ':link',
-            title,
-            contexts: ['link'],
-            targetUrlPatterns: pattern,
-        })
+    chrome.contextMenus.create({
+        id: 'Quiz_Overrides',
+        title: 'Quiz Overrides',
+        documentUrlPatterns: ['*://*/courses/*/quizzes/*'],
+    })
+
+    chrome.contextMenus.create({
+        id: 'Quiz_Overrides:link',
+        title: 'Modify Overrides for this quiz',
+        targetUrlPatterns: ['*://*/courses/*/quizzes/*'],
+    })
+
+    chrome.contextMenus.create({
+        id: 'Moderate_Quiz',
+        title: 'Moderate quiz',
+        documentUrlPatterns: ['*://*/courses/*/quizzes/*'],
+    })
+
+    chrome.contextMenus.create({
+        id: 'Moderate_Quiz:link',
+        title: 'Moderate this quiz',
+        targetUrlPatterns: ['*://*/courses/*/quizzes/*'],
+    })
+
+    chrome.contextMenus.create({
+        id: 'Export_Quiz',
+        title: 'Export quiz (.yml)',
+        documentUrlPatterns: ['*://*/courses/*/quizzes/*'],
+    })
+
+    chrome.contextMenus.create({
+        id: 'Export_Quiz:link',
+        title: 'Export this quiz (.yml)',
+        targetUrlPatterns: ['*://*/courses/*/quizzes/*'],
     })
 })
+
+chrome.contextMenus.onClicked.addListener(onClickHandler)
--- src/content.js
+++ src/content.js
@@ -1,9 +1,66 @@
 import yaml from 'js-yaml'
+import canvasQ from 'question-converter/src/canvas'
+
+// console.log(
+//     yaml.safeDump({
+//         this: 'is a',
+//         95: 'test',
+//         a: ['8', 8],
+//     }),
+// )
 
 console.log(
-    yaml.safeDump({
-        this: 'is a',
-        95: 'test',
-        a: ['8', 8],
-    }),
+    `from content I'm at ${baseUrl}. Trying ${exec_fn}. for ${item_id} on ${course_id}`,
 )
+
+function loadFiles(...files) {
+    console.log(arguments)
+}
+
+const handlers = {
+    Student_List(url, tab) {
+        loadFiles(
+            'assets/axios.v0.19.0.min.js',
+            'downloadCSV.js',
+            'common.js',
+            'exportStudents.js',
+        )
+    },
+    Import_Quiz(url, tab) {},
+    Export_Quiz(url, tab) {
+        loadFiles(
+            'assets/jquery.js',
+            'assets/axios.v0.19.0.min.js',
+            'assets/js-yaml.js',
+            'q_lib.js',
+            'canvas_questions.js',
+            'common.js',
+            'exportQuiz.js',
+        )
+    },
+    Add_Questions(url, tab) {
+        loadFiles(
+            'assets/jquery.js',
+            'assets/js-yaml.js',
+            'quizzes.js',
+            'quizPost.js',
+        )
+    },
+    Moderate_Quiz(url, tab) {
+        loadFiles(
+            'assets/axios.v0.19.0.min.js',
+            'assets/jquery.js',
+            'common.js',
+            'moderate_quiz.js',
+        )
+    },
+    Quiz_Overrides(url, tab) {
+        loadFiles(
+            'assets/axios.v0.19.0.min.js',
+            'assets/jquery.js',
+            'common.js',
+            'quizAssign.js',
+        )
+    },
+}
+handlers[exec_fn]()
