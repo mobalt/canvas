b7aa122a1779137b878a1bd9a8eb3184faf1eb19
Moises Baltazar <me@moisesb.com>
2019-08-08 16:58:25 -0500

    Use webpack to simplify dependency management

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    WriteFilePlugin = require('write-file-webpack-plugin'),
+    yaml = require('js-yaml'),
+    path = require('path')
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+        background: './src/background.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        //     filename: '[name].bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/manifest.yml',
+                to: 'manifest.json',
+                transform: function(content) {
+                    return Buffer.from(
+                        JSON.stringify({
+                            version: process.env.npm_package_version,
+                            ...yaml.safeLoad(content),
+                        }),
+                    )
+                },
+            },
+            {
+                from: 'assets/*',
+                flatten: true,
+            },
+        ]),
+        new WriteFilePlugin(),
+    ],
+    devtool: 'cheap-module-eval-source-map',
+}
