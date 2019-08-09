af2640fc3a403c89599e3a8360acf305af1e1895
Moises Baltazar <me@moisesb.com>
2019-08-08 23:09:06 -0500

    Move today function to helpers.js

--- src/exportQuiz.js
+++ src/exportQuiz.js
@@ -1,17 +1,4 @@
-function pad(n, len = 2) {
-    return String(n).padStart(len, '0')
-}
-function today() {
-    const today = new Date()
-
-    //Having January be 0 will confuse mortals, so normalize it
-    const mm = today.getMonth() + 1,
-        dd = today.getDate(),
-        yyyy = today.getFullYear()
-
-    // pad the month and day with leading zeroes
-    return `${pad(mm)}-${pad(dd)}-${yyyy}`
-}
+import {today} from "./helpers"
 
 // example `7789_students_01-31-2000.csv`
 const filename = `${course_id}_students_${today()}.csv`
--- src/exportStudents.js
+++ src/exportStudents.js
@@ -1,23 +1,12 @@
-function pad(n, len = 2) {
-    return String(n).padStart(len, '0')
-}
-function today() {
-    const today = new Date()
-
-    //Having January be 0 will confuse mortals, so normalize it
-    const mm = today.getMonth() + 1,
-        dd = today.getDate(),
-        yyyy = today.getFullYear()
-
-    // pad the month and day with leading zeroes
-    return `${pad(mm)}-${pad(dd)}-${yyyy}`
-}
+import {today} from "./helpers"
+import {downloadCsv} from "./download"
 
 
 // example `7789_students_01-31-2000.csv`
 const filename = `${course_id}_students_${today()}.csv`
 alert(
-    `Downloading student list to ${filename} Depending on class-size, might take several seconds.`,
+    `Downloading student list to ${filename}.
+     Depending on class-size, might take several seconds.`,
 )
 
 // Students have enrollment_role_id == 3, filter out anyone else
